2243bd67535732beb6222d5547598b6ce04baaa1
Nugine <nugine@foxmail.com>
2024-07-14 17:10:49 +0800

    memtier

--- scripts/memtier.sh
+++ scripts/memtier.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -ex
 
-memtier_benchmark --random-data --command="pfadd key1 __data__" -n 10000 --hdr-file-prefix "key1_add"
-memtier_benchmark --random-data --command="pfadd key2 __data__" -n 10000 --hdr-file-prefix "key2_add"
-memtier_benchmark --random-data --command="pfadd key3 __data__" -n 10000 --hdr-file-prefix "key3_add"
+echo > stdout.log
+memtier_benchmark --random-data --command="pfadd key1 __data__" -n 10000 --hdr-file-prefix "key1_add" >> stdout.log
+memtier_benchmark --random-data --command="pfadd key2 __data__" -n 10000 --hdr-file-prefix "key2_add" >> stdout.log
+memtier_benchmark --random-data --command="pfadd key3 __data__" -n 10000 --hdr-file-prefix "key3_add" >> stdout.log
 
-memtier_benchmark --command="pfcount key1" -n 10000 --hdr-file-prefix "key1_count"
+memtier_benchmark --command="pfcount key1" -n 10000 --hdr-file-prefix "key1_count" >> stdout.log
 
-memtier_benchmark --command="pfmerge keyall key1 key2 key3" -n 10000 --hdr-file-prefix "key_merge"
+memtier_benchmark --command="pfmerge keyall key1 key2 key3" -n 10000 --hdr-file-prefix "key_merge" >> stdout.log
 
 # cleanup
 redis-cli del key1 key2 key3 keyall
